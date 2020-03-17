0b4c78ab4e753440b468591deaa94951a16a5a36
Sidaartha Reddy <sidaarthareddy1998@gmail.com>
2020-03-17 20:37:48 +0530

    added all keys

--- Scripts/script_to_get_covid19_india_data.py
+++ Scripts/script_to_get_covid19_india_data.py
@@ -21,21 +21,22 @@ print("scraping initiated")
 
 info = soup.find('ol')
 points = info.findAll('strong')
-air_screening = points[0].text
-total_cases = points[1].text.split(':')[1].split()[0]
-updated_time = points[1].text.split('as on')[1].split(')')[0][1:]
+air_screening = points[0].text.split(':')[1].split()[0].replace(',', '')
+total_active_cases = points[1].text.split(':')[1].split()[0]
+updated_time = info.findAll('p')[-1].text.split('as on')[1].split(')')[0][1:]
 
 table = info.find('tbody')
 rows = table.findAll('tr')
 totals = rows[-1].findAll('td')
 
 data_obj = {}
-data_obj['screened_at_airport'] = air_screening
-data_obj['total_cases'] = total_cases
-data_obj['total_domestic_cases'] = totals[1].text
-data_obj['total_international_cases'] = totals[2].text
-data_obj['total_cured_cases'] = totals[3].text
-data_obj['total_death_cases'] = totals[4].text
+data_obj['screened_at_airport'] = int(air_screening)
+data_obj['total_cases'] = int(total_active_cases) + int(totals[3].text) + int(totals[4].text)
+data_obj['total_active_cases'] = int(total_active_cases)
+data_obj['total_domestic_cases'] = int(totals[1].text)
+data_obj['total_international_cases'] = int(totals[2].text)
+data_obj['total_cured_cases'] = int(totals[3].text)
+data_obj['total_death_cases'] = int(totals[4].text)
 data_obj['last_updated_time'] = updated_time
 data_obj['states'] = {}
 print("all total counts done")
@@ -43,12 +44,12 @@ print("all total counts done")
 for row in rows[:-1]:
 	cols = row.findAll('td')
 	data_obj['states'][cols[1].text] = {
-		'sl.no' : cols[0].text,
+		'sl.no' : int(cols[0].text),
 		'name' : cols[1].text,
-		'domestic_cases' : cols[2].text,
-		'international_cases' : cols[3].text,
-		'cured_cases' : cols[4].text,
-		'death_cases' : cols[5].text
+		'domestic_cases' : int(cols[2].text),
+		'international_cases' : int(cols[3].text),
+		'cured_cases' : int(cols[4].text),
+		'death_cases' : int(cols[5].text)
 	}
 	print(cols[1].text+" done")
 print("scraping finished")
